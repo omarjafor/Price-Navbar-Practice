344d44fd9cbb00e66f2c124553fcfdf22a1bcc3f
omarjafor <omarjaforchy@gmail.com>
2023-09-18 22:58:05 +0600

    working on navbar and card

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -24,15 +24,7 @@ const Navbar = () => {
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
                         <li><a>Item 1</a></li>
-                        <li tabIndex={0}>
-                            <details>
-                                <summary>Item 2</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
+                        <li><a>Item 2</a></li>
                         <li><a>Item 3</a></li>
                     </ul>
                 </div>
