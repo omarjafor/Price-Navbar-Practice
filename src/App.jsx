5d0b1656014fc78894b48cbb78cde845485b124b
omarjafor <omarjaforchy@gmail.com>
2023-09-18 13:36:25 +0600

    project basic stracture setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-black font-bold rounded-lg py-2 bg-lime-600">
+        React Navbar Price 
+      </h1>
+    </>
+  )
+}
+
+export default App
