6f36a26493ddd6a40d918f162a8743db40eb2427
omarjafor <omarjaforchy@gmail.com>
2023-09-18 22:44:47 +0600

    react card design working

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Card from './Card'
 
 function App() {
 
@@ -7,16 +8,7 @@ function App() {
       <h1 className="text-3xl text-black font-bold rounded-lg py-2 bg-lime-600">
         React Navbar Price 
       </h1>
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure><img src="https://www.skechers.in/dw/image/v2/BGNZ_PRD/on/demandware.static/-/Sites-skechersin-Library/default/dw0e2e3398/Category-Landing/Category-Desktop/Mens/Desktop_Men_Walking_Shoes.jpg?sw=780" alt="Shoes" /></figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
+      <Card></Card>
     </>
   )
 }
---
+++ src/Card.jsx
@@ -0,0 +1,18 @@
+const Card = () => {
+    return (
+        <div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure><img src="https://www.skechers.in/dw/image/v2/BGNZ_PRD/on/demandware.static/-/Sites-skechersin-Library/default/dw0e2e3398/Category-Landing/Category-Desktop/Mens/Desktop_Men_Walking_Shoes.jpg?sw=780" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
