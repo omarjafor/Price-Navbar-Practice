ed80acf3c5a69828644e31cb0eff1512c5646dd7
omarjafor <omarjaforchy@gmail.com>
2023-09-18 22:56:50 +0600

    working on navbar and card

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -19,14 +19,14 @@ const Navbar = () => {
                             <li><a>Item 3</a></li>
                         </ul>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                    <a className="btn btn-ghost normal-case text-xl">OmarJafor</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
                         <li><a>Item 1</a></li>
                         <li tabIndex={0}>
                             <details>
-                                <summary>Parent</summary>
+                                <summary>Item 2</summary>
                                 <ul className="p-2">
                                     <li><a>Submenu 1</a></li>
                                     <li><a>Submenu 2</a></li>
