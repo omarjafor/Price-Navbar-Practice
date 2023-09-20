58c077e910e6ef665f9bc81109d95cc3b7e60576
omarjafor <omarjaforchy@gmail.com>
2023-09-20 09:43:17 +0600

    price option design

--- src/PriceOption/PriceOption.jsx
+++ src/PriceOption/PriceOption.jsx
@@ -3,15 +3,17 @@ import Feature from "./Feature";
 const PriceOption = ({ option }) => {
     const {name, price, features} = option;
     return (
-        <div className="bg-lime-800 text-white rounded-lg py-4 relative">
+        <div className="bg-lime-800 text-white flex items-center flex-col rounded-lg py-4 relative">
             <h2 className="font-bold mb-3">
                 <span className="text-4xl"> {price} </span>
             </h2>
             <h4 className="text-2xl font-bold"> {name} </h4>
-            {
-                features.map((feature, idx) => <Feature key={idx} feature={feature}
-                ></Feature>)
-            }
+            <div className="flex-grow">
+                {
+                    features.map((feature, idx) => <Feature key={idx} feature={feature}
+                    ></Feature>)
+                }
+            </div>
             <button className="bg-fuchsia-600 w-2/4 rounded-xl p-3 font-bold">Subscribe Now</button>
         </div>
     );
