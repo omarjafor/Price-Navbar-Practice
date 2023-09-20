1ab1fba3eaf3733568bd7afe5c0025f049b7f4dd
omarjafor <omarjaforchy@gmail.com>
2023-09-20 11:02:38 +0600

    axios data loading

--- src/Phones/Phones.jsx
+++ src/Phones/Phones.jsx
@@ -1,12 +1,26 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 const Phones = () => {
     const [phones, setPhones] = useState([]);
 
     useEffect( ()=>{
-        fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
-        .then(res => res.json())
-        .then(data => setPhones(data.data))
+        // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+        // .then(res => res.json())
+        // .then(data => setPhones(data.data))
+
+        axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
+        .then(data => {
+            const phoneData = data.data.data;
+            const phonesFakeData = phoneData.map(phone => {
+                const obj = {
+                    name: phone.phone_name,
+                    price: parseInt(phone.slug.split('-')[1])
+                }
+                return obj;
+            })
+            setPhones(phonesFakeData);
+        });
     },[])
     
     return (
